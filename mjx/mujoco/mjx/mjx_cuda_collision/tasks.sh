9834aa10463336f0c03554bcd78fd63f3385bfb8
btaba <7320238+btaba@users.noreply.github.com>
2024-10-17 16:31:37 -0700

    add ptx and cubin

--- mjx/mujoco/mjx/mjx_cuda_collision/tasks.sh
+++ mjx/mujoco/mjx/mjx_cuda_collision/tasks.sh
@@ -9,7 +9,13 @@ export CMAKE_LIBRARY_PATH=/opt/cudnn/8.9.7/lib:/opt/cuda/12.2.0/targets/x86_64-l
 
 g++-12 -fPIC -c mjx_cuda_collision.xla.ffi.cc -o mjx_cuda_collision.xla.ffi.cc.o -I/usr/local/cuda/targets/x86_64-linux/include/ -I${SITE_PACKAGES}/jaxlib/include/ -I${SITE_PACKAGES}/tensorflow/include/external/pybind11/include/ -I${SITE_PACKAGES}/tensorflow/include/external/local_config_python/python_include/ -I${SITE_PACKAGES}/mujoco/include && \
                 g++-12 -fPIC -c mjx_cuda_collision.common.cpp -o mjx_cuda_collision.common.cpp.o -I/usr/local/cuda/targets/x86_64-linux/include/ -I${SITE_PACKAGES}/jaxlib/include/ -I${SITE_PACKAGES}/tensorflow/include/external/pybind11/include/ -I${SITE_PACKAGES}/tensorflow/include/external/local_config_python/python_include/ -I${SITE_PACKAGES}/mujoco/include/ && \
-                nvcc -c -Xcompiler -fPIC mjx_cuda_collision.cu -o mjx_cuda_collision.cu.o -I/usr/local/cuda/targets/x86_64-linux/include/ -I${SITE_PACKAGES}/jaxlib/include/ -I${SITE_PACKAGES}/tensorflow/include/external/pybind11/include/ -I${SITE_PACKAGES}/tensorflow/include/external/local_config_python/python_include/ -I${SITE_PACKAGES}/mujoco/include/ && \
+                nvcc -c -Xcompiler -fPIC -ccbin /usr/bin/g++-12  mjx_cuda_collision.cu -o mjx_cuda_collision.cu.o -I/usr/local/cuda/targets/x86_64-linux/include/ -I${SITE_PACKAGES}/jaxlib/include/ -I${SITE_PACKAGES}/tensorflow/include/external/pybind11/include/ -I${SITE_PACKAGES}/tensorflow/include/external/local_config_python/python_include/ -I${SITE_PACKAGES}/mujoco/include/ && \
                 g++-12 -shared -o _mjx_cuda_collision.so mjx_cuda_collision.xla.ffi.cc.o mjx_cuda_collision.common.cpp.o mjx_cuda_collision.cu.o -L${SITE_PACKAGES}/tensorflow/ -ltensorflow_cc -L/usr/local/cuda/targets/x86_64-linux/lib/ -lcudart
 
 cp *.so ../
+
+# generate cubin
+# nvcc -c -Xcompiler -fPIC -ccbin /usr/bin/g++-12 -cubin mjx_cuda_collision.cu -o mjx_cuda_collision.cubin -I/usr/local/cuda/targets/x86_64-linux/include/ -I${SITE_PACKAGES}/jaxlib/include/ -I${SITE_PACKAGES}/tensorflow/include/external/pybind11/include/ -I${SITE_PACKAGES}/tensorflow/include/external/local_config_python/python_include/ -I${SITE_PACKAGES}/mujoco/include/
+
+# generate ptx
+# nvcc -c -Xcompiler -fPIC -ccbin /usr/bin/g++-12  mjx_cuda_collision.cu -ptx   -I/usr/local/cuda/targets/x86_64-linux/include/ -I${SITE_PACKAGES}/jaxlib/include/ -I${SITE_PACKAGES}/tensorflow/include/external/pybind11/include/ -I${SITE_PACKAGES}/tensorflow/include/external/local_config_python/python_include/ -I${SITE_PACKAGES}/mujoco/include/
